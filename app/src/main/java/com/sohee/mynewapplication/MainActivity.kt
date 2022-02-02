f7a2a102a5b0262528a95d4f956d658ae0818d09
cowha <cowhappy721@naver.com>
2022-02-02 19:39:04 +0900

    add from sohee

--- app/src/main/java/com/sohee/mynewapplication/MainActivity.kt
+++ app/src/main/java/com/sohee/mynewapplication/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.sohee.mynewapplication
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Base64
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.sohee.mynewapplication.databinding.ActivityMainBinding
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +35,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/sohee/mynewapplication/SecondFragment.kt
+++ app/src/main/java/com/sohee/mynewapplication/SecondFragment.kt
@@ -35,6 +35,8 @@ class SecondFragment : Fragment() {
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_ThirdFragment) }
     }
 
     override fun onDestroyView() {
